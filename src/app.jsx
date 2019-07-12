0116983321af604d823939cfdd840e6cd4bc9f3f
Gilbert Awaji-Mitop <gilbertawaji-mitop@Gilberts-MacBook-Pro.local>
2019-07-12 09:12:42 +0100

    setup(chore): webpack
    setup webpack
    setup package.json
    
    [finishes #167246651]

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <h1>WELCOME TO QUICK-CREDIT REACT</h1>
+        )
+    }
+}
+
+export default App;
